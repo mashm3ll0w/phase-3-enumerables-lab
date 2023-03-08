c806d9c01b21784ce03966ccb8b1109f42198d9e
Charles Swaleh <charlswall@live.com>
2023-03-08 22:01:48 +0300

    Completed the get_names method

--- enumerables.rb
+++ enumerables.rb
@@ -13,6 +13,7 @@ end
 # with the names of each spicy food
 def get_names(spicy_foods)
   # your code here
+  spicy_foods.map {|food| food[:name]}
 end
 
 # given an array of spicy foods, **return an array of hashes** 
